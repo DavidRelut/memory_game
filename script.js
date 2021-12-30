a578746f3866a9853e8e9640160016cab5bb7d12
David R <codingaccelerator@gmail.com>
2021-12-30 20:17:28 +0100

    Update visibility cards and display all the cards on the DOM with Javascript

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 var groupCard = ['🦄', '🍦', '🌈', '👽', '👾', '🤖', '👹', '👺'];
 var totalCard = groupCard.concat(groupCard);
 
-const container = document.getElementById('container');
-const card = document.getElementById('card');
-const cardContent =  document.getElementById('card_content');
+// const container = document.querySelector('.container');
+// const card = document.getElementById('card');
+// const cardContent =  document.getElementById('card_content');
 
+function distributeCard(element){
+    const container = document.getElementById('container');
+
+    container.innerHTML = "";
+
+    for(let i = 0; i < element.length; i++){
+        let cards = document.createElement('div');
+
+        cards.innerHTML = `
+        <div class="card">
+            <div class="card__content">
+                ${element[i]}
+            </div>
+        </div>`
+
+        container.appendChild(cards);
+    }
+}
+
+distributeCard(totalCard);
