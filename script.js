52cec5ccdd1c23a706a24b0659f0dfbdbabd52e3
David R <codingaccelerator@gmail.com>
2021-12-30 19:26:06 +0100

    Create Cards + init script.js and style of Cards and interaction

---
+++ script.js
@@ -0,0 +1,7 @@
+var groupCard = ['🦄', '🍦', '🌈', '👽', '👾', '🤖', '👹', '👺'];
+var totalCard = groupCard.concat(groupCard);
+
+const container = document.getElementById('container');
+const card = document.getElementById('card');
+const cardContent =  document.getElementById('card_content');
+
